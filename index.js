a7547e27d82b0460beadb71272ce02353c9655c4
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-27 19:53:49 +0600

    addUser api created for post data

--- index.js
+++ index.js
@@ -13,12 +13,26 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pzlo6ar.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         const userCollection = client.db("resaleBooks").collection("userCollection");
+
+        app.post("/addUser", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const data = await userCollection.find(query).toArray();
+            if (data.length === 0) {
+                const result = await userCollection.insertOne(user);
+                res.send(result);
+                console.log(result);
+            } else {
+                res.send("User Already added");
+                console.log("User Already added");
+            }
+        });
+
     }
     finally {
 
