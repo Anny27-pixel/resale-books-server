b7810f2224f07a8929aeba55eb3c7793520baeb4
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-27 18:32:10 +0600

    mongodb database added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,6 +10,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pzlo6ar.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const userCollection = client.db("resaleBooks").collection("userCollection");
+    }
+    finally {
+
+    }
+}
+run().catch((error) => {
+    console.error(error);
+});
 
 
 app.get('/', async (req, res) => {
